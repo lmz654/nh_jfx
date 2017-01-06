850b17912da04a02271b5e4ca7b96bc27def5f04
lmz654 <huudwc@gmail.com>
2017-01-05 21:46:32 -0600

    modified frame

--- java_rest/src/Model/Transaction.java
+++ java_rest/src/Model/Transaction.java
@@ -3,22 +3,38 @@ package Model;
 import java.util.Date;
 
 public class Transaction {
+	private int type;
 	private String txnumber;
 	private Date txdate;
 	private String Description;
 	private Double amount;
 	
+	private String cardnumber;
+	private Date edate;
+	private String verifiednum;
+	private String nameoncard;
+	
 	public Transaction(){
 		// TODO Auto-generated constructor stub
 	}
 
-	public Transaction(String txnumber, Date txdate,String description, Double amount) {
+	
+
+	public Transaction(int tupe, String txnumber, Date txdate, String description, Double amount, String cardnumber, Date edate,
+			String verifiednum, String nameoncard) {
 		super();
+		this.type = type;
 		this.txnumber = txnumber;
 		this.txdate = txdate;
 		Description = description;
-		this.amount=amount;
+		this.amount = amount;
+		this.cardnumber = cardnumber;
+		this.edate = edate;
+		this.verifiednum = verifiednum;
+		this.nameoncard = nameoncard;
 	}
+
+
 
 	public String getTxnumber() {
 		return txnumber;
@@ -50,6 +66,66 @@ public class Transaction {
 
 	public void setAmount(Double amount) {
 		this.amount = amount;
+	}
+
+
+
+	public int getType() {
+		return type;
+	}
+
+
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+
+
+	public String getCardnumber() {
+		return cardnumber;
+	}
+
+
+
+	public void setCardnumber(String cardnumber) {
+		this.cardnumber = cardnumber;
+	}
+
+
+
+	public Date getEdate() {
+		return edate;
+	}
+
+
+
+	public void setEdate(Date edate) {
+		this.edate = edate;
+	}
+
+
+
+	public String getVerifiednum() {
+		return verifiednum;
+	}
+
+
+
+	public void setVerifiednum(String verifiednum) {
+		this.verifiednum = verifiednum;
+	}
+
+
+
+	public String getNameoncard() {
+		return nameoncard;
+	}
+
+
+
+	public void setNameoncard(String nameoncard) {
+		this.nameoncard = nameoncard;
 	}
 	
 }
---
+++ java_rest/src/View/Login_Scene.java
@@ -0,0 +1,44 @@
+package View;
+
+import Model.Employee;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.paint.Paint;
+
+public class Login_Scene extends Scene{
+	
+	private Employee employee;
+
+	public Login_Scene(Parent arg0, double arg1, double arg2, boolean arg3, SceneAntialiasing arg4) {
+		super(arg0, arg1, arg2, arg3, arg4);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Login_Scene(Parent arg0, double arg1, double arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Login_Scene(Parent arg0, double arg1, double arg2, Paint arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Login_Scene(Parent arg0, double arg1, double arg2) {
+		super(arg0, arg1, arg2);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Login_Scene(Parent arg0, Paint arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Login_Scene(Parent arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
