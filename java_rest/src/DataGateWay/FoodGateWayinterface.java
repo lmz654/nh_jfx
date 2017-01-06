47915e92ebba92648553dd81dde05a038fa44dca
lmz654 <huudwc@gmail.com>
2017-01-06 17:40:07 -0600

    add more

--- java_rest/src/DataGateWay/FoodGateWayinterface.java
+++ java_rest/src/DataGateWay/FoodGateWayinterface.java
@@ -1,5 +1,14 @@
 package DataGateWay;
 
-public class FoodGateWayinterface {
+import java.util.List;
 
+import Node.FoodButton;
+import Node.TableButton;
+
+public interface FoodGateWayinterface {
+	List<FoodButton> FetchFood() throws Exception;
+	void UpdateTable(TableButton table) throws Exception;
+	void DelleteTable(TableButton table) throws Exception;
+	void AddTable(TableButton table) throws Exception;
+	void close() throws Exception;
 }
--- java_rest/src/DataGateWay/TableGateWayinterface.java
+++ java_rest/src/DataGateWay/TableGateWayinterface.java
@@ -2,11 +2,12 @@ package DataGateWay;
 
 import java.util.List;
 
-import Node.Table;
+import Node.TableButton;
 
 public interface TableGateWayinterface {
-	List<Table> FetchTable() throws Exception;
-	void UpdateTable(Table table) throws Exception;
-	void DelleteTable(Table table) throws Exception;
-	void AddTable(Table table) throws Exception;
+	List<TableButton> FetchTable() throws Exception;
+	void UpdateTable(TableButton table) throws Exception;
+	void DelleteTable(TableButton table) throws Exception;
+	void AddTable(TableButton table) throws Exception;
+	void close() throws Exception;
 }
